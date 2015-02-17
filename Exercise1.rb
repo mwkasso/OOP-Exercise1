437b2ca179867671c3f6f38dfcab3d390023ffe1
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-17 15:53:44 +0000

    Added print_details method to Person class

--- Exercise1.rb
+++ Exercise1.rb
@@ -1,7 +1,8 @@
 class Person
 	attr_reader :first_name, :surname, :dob, :emails, :phone_numbers
 	
-	def initialize(name, surname, dob = "")
+	#initialize
+	def initialize(name = "joe", surname = "bloggs", dob = "")
 		@first_name = name.capitalize
 		@surname = surname.capitalize
 		@dob = dob
@@ -9,9 +10,28 @@ class Person
 		@phone_numbers = []
 	end
 
+	# get methods
+
 	def fullname
 		return @first_name + " " +@surname
 	end
+
+	def print_details
+		puts "#{fullname}\n"
+		puts "-" * fullname.length
+		puts "Date of Birth: " + @dob.to_s + "\n\n"
+		puts "Email Addresses:\n"
+		@emails.each do |value|
+			puts "- " + value
+		end
+		puts "Phone Numbers:\n"
+		@phone_numbers.each do |value|
+			puts "- " + value
+		end
+		return ''
+	end
+
+	# set methods
 
 	def add_email(email)
 		@emails << email
@@ -28,6 +48,8 @@ class Person
 	def remove_phone(position)
 		@phone_numbers.delete_at(position)
 	end
+
+	# methods overrided
 
 	def to_s
 		return "#{fullname} was born on #{@dob}. \nTheir email addresses are: #{@emails}. \nTheir phone numbers are #{@phone_numbers}"
