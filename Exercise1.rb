76d6de64fc2a032f93c04261a249f46e2c456029
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-17 14:53:20 +0000

    Added a add_email method into Person class

--- Exercise1.rb
+++ Exercise1.rb
@@ -1,8 +1,16 @@
 class Person
 	attr_accessor :name, :surname, :dob, :emails
+	
 	def initialize(name, surname, dob = "")
 		@name = name
 		@surname = surname
 		@dob = dob
+		@emails = []
 	end
+
+	def add_email(email)
+		@emails << email
+	end
+
+	
 end
