28733fb9031de885c4f48d74cfdea33e6a9c635f
Mohammed Kasso <mkasso@spartaglobal.com>
2015-02-17 14:58:44 +0000

    Added phone_number variable with a add_phone method to Person class

--- Exercise1.rb
+++ Exercise1.rb
@@ -1,16 +1,19 @@
 class Person
-	attr_accessor :name, :surname, :dob, :emails
+	attr_accessor :name, :surname, :dob, :emails, :phone_numbers
 	
 	def initialize(name, surname, dob = "")
 		@name = name
 		@surname = surname
 		@dob = dob
 		@emails = []
+		@phone_num = []
 	end
 
 	def add_email(email)
 		@emails << email
 	end
 
-	
+	def add_phone(phone)
+		@phone_numbers << phone
+	end
 end
